1348ff98aedd21b69d2ddc36a04cd80fc53adf6b
mcc <costanza+git@gmail.com>
2021-06-03 12:45:53 -0700

    accept new note via post, add id and store; fix add note button

--- server.js
+++ server.js
@@ -23,11 +23,25 @@ app.get('/api/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'db/db.json'));
 });
 
-app.post('/api/note', ({ body }, res) => {
-  console.log('adding note ', + body);
-}); 
+app.post('/api/notes', ({ body }, res) => {
+  const newNote = body;
+  newNote.id = 1 + notes.reduce((acc, a) => Math.max(acc, a.id), 0);
+  notes.push(newNote);
+  fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+    if (err) {
+      console.error(err.message);
+      res.status(500).json({
+        error: err.message
+      });
+    }
+    res.json({
+      message: 'saved new note',
+      note: newNote
+    });
+  });
+});
 
-app.delete('/api/note/:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
   console.log('deleting note ' + req.params.id);
 }); 
 
