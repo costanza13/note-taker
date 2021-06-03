3b25f2feab1457a342ee1b98dee5004ad1527489
mcc <costanza+git@gmail.com>
2021-06-03 15:58:57 -0700

    move routes out of server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const fs = require('fs');
 const path = require('path');
+const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,64 +11,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-let notes = [];
-
-// html routes
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
-});
-
-// api routes
-app.get('/api/notes', (req, res) => {
-  notes = JSON.parse(fs.readFileSync(path.join(__dirname, 'db/db.json')));
-  res.json(notes);
-});
-
-app.post('/api/notes', ({ body }, res) => {
-  const newNote = body;
-  newNote.id = 1 + notes.reduce((acc, a) => Math.max(acc, a.id), 0);
-  notes.push(newNote);
-  fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-    if (err) {
-      console.error(err.message);
-      res.status(500).json({
-        error: err.message
-      });
-    }
-    res.json({
-      message: 'saved new note',
-      note: newNote
-    });
-  });
-});
-
-app.delete('/api/notes/:id', (req, res) => {
-  // find note by id
-  let i = 0;
-  while (i < notes.length) {
-    if (notes[i].id === parseInt(req.params.id)) {
-      break;
-    }
-    i++;
-  }
-  if (i < notes.length) {
-    notes.splice(i, 1);
-    fs.writeFile(path.join(__dirname, 'db/db.json'), JSON.stringify(notes, null, 2), (err) => {
-      if (err) {
-        console.error(err.message);
-        res.status(500).json({
-          error: err.message
-        });
-      }
-      res.json({
-        message: 'deleted note',
-        id: i
-      });
-    });
-  } else {
-    res.status(404).json({ error: 'note note found' });
-  }
-});
+// use apiRoutes
+app.use('/', routes);
 
 // default response for any other request (Not Found)
 app.use((req, res) => {
